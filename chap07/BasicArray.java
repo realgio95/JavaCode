489388851b9644f6acc3be6d14bcc4f665b2a6a9
realgio95 <realgio95@hotmail.com>
2015-10-31 02:47:46 -0500

    Java Code Added Which I have been practising from

---
+++ chap07/BasicArray.java
@@ -0,0 +1,37 @@
+//********************************************************************
+//  BasicArray.java       Author: Lewis/Loftus
+//
+//  Demonstrates basic array declaration and use.
+//********************************************************************
+
+public class BasicArray
+{
+   //-----------------------------------------------------------------
+   //  Creates an array, fills it with various integer values,
+   //  modifies one value, then prints them out.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+      final int LIMIT = 15, MULTIPLE = 10;
+
+      int[] list = new int[LIMIT];
+      
+      //  Initialize the array values
+      for (int index = 0; index < LIMIT; index++)
+         list[index] = index * MULTIPLE;
+      
+      list[5] = 999;  // change one array value
+      
+      //  Print the array values
+      for (int value : list)
+         System.out.print (value + "  ");
+
+	int value;
+      for (int i = 0; i < LIMIT; i++) 
+	{
+         value = list[i];
+         System.out.print (value + "  ");
+	}
+
+   }
+}
